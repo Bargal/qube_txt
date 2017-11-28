5f29bf5e66f1e2d5316262d1120921a2dfa8bc32
Bartosz Gala <bartosz.gala@mail.ru>
2017-11-28 22:17:47 +0100

    powrot do struktury jedno plikowej

--- qube_txt/comm_txt.h
+++ qube_txt/comm_txt.h
@@ -4,5 +4,3 @@ void txtMain();
 //wyswietlanie tekstu wejsciowego na plansze
 void txtInside(std::string, std::string);
 
-//grafika tekstowa - wyglad pokoju z uwzglednieniem drzwi.
-void graphQ(gdata *p_allData, qube **p_p_matrix);
--- qube_txt/initialize.h
+++ qube_txt/initialize.h
@@ -1,9 +1,1 @@
 #pragma once
-//inicjacja i wprowadzanie danych do tablicy struktur planszy gry
-void defineQ(gdata* p_allData, qube** p_p_matrix, int, int);
-
-//realizacja wysokiej trudnosci gry
-void hardmode(gdata* p_allData, qube** p_p_matrix, int, int);
-
-//inicjacja HP i Time
-void hpTimeInit(gdata* p_allData);
