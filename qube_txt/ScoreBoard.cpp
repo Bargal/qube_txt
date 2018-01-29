b0e2b66d1810e9ce16d26783935977e6df07a426
Bartosz Gala <bartosz.gala@mail.ru>
2018-01-29 14:58:28 +0100

    Score board development...

--- qube_txt/ScoreBoard.cpp
+++ qube_txt/ScoreBoard.cpp
@@ -4,12 +4,41 @@
 #include <string>;
 #include <iostream>;
 
-ScoreBoard::ScoreBoard() : current(0), rank(0), filestatus(true)
+ScoreBoard::ScoreBoard() : filestatus(true)
 {
 	std::ifstream scoreFile("score.dat");
 	if (!scoreFile.is_open()) filestatus = false;
+	else
+	{
+		for (int i = 0; i < 10; i++)
+			scoreFile >> score[i];
+
+		//sorting table of scores
+	}
 }
 
 ScoreBoard::~ScoreBoard()
 {
+	std::ofstream scoreFile("score.dat");
+	if (!scoreFile.is_open()) filestatus = false;
+	else
+	{
+		for (int i = 0; i < 10; i++)
+			scoreFile << score[i];
+	}
+}
+
+void ScoreBoard::showScoreBoard(const int current)
+{
+	int position = 1;
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "Miejsce " << i + 1 << " - " << score[i] << "  punkty" << std::endl;
+
+		if (score[i] > current) position++;
+
+		std::cout << "Twoj wynik " << current << " zapewnia ci " << position << " miejsce" << std::endl;
+
+		score[9] = current;
+	}
 }
--- qube_txt/ScoreBoard.h
+++ qube_txt/ScoreBoard.h
@@ -1,16 +1,16 @@
 #pragma once
 
-/**
- * \brief
+/*
+ * current it is actual game score.
  */
 class ScoreBoard
 {
-	int score[10]{ 0 };  //table of top 10 game scores
-	int current; //the result of the current game
-	int rank; //ranking of the current game
+	int score[10]; //table of top 10 game scores
 	bool filestatus; //file read status
 
 public:
 	ScoreBoard();
 	~ScoreBoard();
+	//show score board on screen and add on end of score table
+	void showScoreBoard(int);
 };
